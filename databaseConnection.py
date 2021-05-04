6b27b22b19d3be97da178ab7b34fc0e7fcbc8a2e
Sunde99 <59199436+Sunde99@users.noreply.github.com>
2021-05-04 16:27:22 +0200

    Added connection to Pokemon collection

--- databaseConnection.py
+++ databaseConnection.py
@@ -3,14 +3,12 @@ import pymongo
 from discord.ext import commands
 from pymongo import MongoClient
 
-
-async def getDB(cluster):
-    db = cluster["Pokebot"]
-    return db
-
 async def pointsDB(db):
-    points = db["Points"]
-    return points
+    return db["Points"]
+    
+
+async def pokemonDB(db):
+    return db["Pokemon"]
 
 async def givePoints(msg, cluster):
     """[Gives the sender of the message a point]
@@ -19,18 +17,18 @@ async def givePoints(msg, cluster):
         msg ([context]): [The person sending the message]
         cluster ([Database Cluster]): [The cluster used for picking out the points table]
     """
-    points = await pointsDB(await getDB(cluster))
+    db = await pointsDB(cluster)
     my_query = { "_id": msg.author.id }
-    if (points.count_documents(my_query) == 0):
+    if (db.count_documents(my_query) == 0):
         post = {"_id": msg.author.id, "score":1}
-        points.insert_one(post)
+        db.insert_one(post)
         print("AAA GIVE POINTS JUST GAVE SOMEONE THEIR FIRST POINT")
     else:
-        user = points.find(my_query)
+        user = db.find(my_query)
         for result in user:
             score = result["score"]
         score += 1
-        points.update_one({"_id":msg.author.id}, {"$set":{"score":score}})
+        db.update_one({ "_id":msg.author.id }, { "$set":{ "score":score } })
         print("AAA POINTS HAVE BEEN INCREASED")
 
 async def showPoints(ctx, cluster):
@@ -43,7 +41,7 @@ async def showPoints(ctx, cluster):
     Returns:
         [Int]: [Points]
     """
-    points = await pointsDB(await getDB(cluster))
+    points = await pointsDB(cluster)
 
     currentPoints = points.find({}, {'_id': 1, 'score': 1})
     for data in currentPoints:
@@ -55,5 +53,19 @@ async def showPoints(ctx, cluster):
         await ctx.channel.send(f"{ctx.author.name} has {score} points")
 
 async def resetPoints(ctx, cluster):
-    points = await pointsDB(await getDB(cluster))
-    points.update_one({"_id":ctx.author.id}, {"$set":{"score":0}})
+    db = await pointsDB(cluster)
+    db.update_one({"_id":ctx.author.id}, {"$set":{"score":0}})
+
+async def catchPokemon(ctx, pokemon, IVs, cluster):
+    db = await pokemonDB(cluster)
+    my_query = { "Trainer": ctx.author.id }
+
+    print("One")
+    amount_of_pokemon = db.count_documents(my_query)
+    _id = str(ctx.author.id) + "-" + str(amount_of_pokemon)
+    print(_id)
+    print("Two")
+
+    post = { "_id": _id, "Trainer": ctx.author.id, "Pokemon": pokemon, "IVs": IVs }
+    print("Three")
+    db.insert_one(post)
