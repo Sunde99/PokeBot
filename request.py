baf00ccd3ed2b0ebf7cfdacb6491c967ae651948
Sunde99 <mssunde1999@gmail.com>
2021-05-05 19:41:40 +0200

    Added item searching

--- request.py
+++ request.py
@@ -15,7 +15,25 @@ def check_status(url):
         return False
     return True
 
+async def getItem(ctx, item):
+    url = "https://pokeapi.co/api/v2/item/" + item
+    if not check_status(url):
+        await ctx.channel.send("something went wrong")
+        return -1
+    
+    return requests.get(url).json()
+
+
 async def getPokemon(ctx, pokeID):
+    """[Gets the pokemon from the api]
+
+    Args:
+        ctx ([context]): [The person sending the message]
+        pokeID ([Int]): [The dex number of the pokemon]
+
+    Returns:
+        [Json]: [A dict of the info about the pokemon]
+    """
     url = "https://pokeapi.co/api/v2/pokemon/" + str(pokeID)
     print(url)
     if not check_status(url):
