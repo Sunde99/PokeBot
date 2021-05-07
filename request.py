a46cecb5c86da6f99527ad66b1511f0d9f3d2bd0
Sunde99 <mssunde1999@gmail.com>
2021-05-08 00:31:01 +0200

    Updated error messages

--- request.py
+++ request.py
@@ -15,21 +15,23 @@ def check_status(url):
         return False
     return True
 
+
 async def getItem(ctx, item):
     url = "https://pokeapi.co/api/v2/item/" + item
     if not check_status(url):
-        await ctx.channel.send("something went wrong")
+        await ctx.channel.send("That's not an item, check your spelling/spacing. Some items can't be found")
         return -1
     
     return requests.get(url).json()
 
 
-async def getPokemon(ctx, pokeID):
+async def getPokemon(ctx, pokeID, give_error=True):
     """[Gets the pokemon from the api]
 
     Args:
         ctx ([context]): [The person sending the message]
         pokeID ([Int]): [The dex number of the pokemon]
+        give_error (Bool, optional]: [If this should throw an error if not found]
 
     Returns:
         [Json]: [A dict of the info about the pokemon]
@@ -37,7 +39,7 @@ async def getPokemon(ctx, pokeID):
     url = "https://pokeapi.co/api/v2/pokemon/" + str(pokeID)
     print(url)
     if not check_status(url):
-        await ctx.channel.send("something went wrong")
+        if (give_error): await ctx.channel.send("something went wrong")
         return -1
 
     pokemon = requests.get(url).json()
@@ -45,10 +47,11 @@ async def getPokemon(ctx, pokeID):
 
     # await ctx.channel.send(pokemon["name"])
 
+
 async def getAbility(ctx, ability):
     url = "https://pokeapi.co/api/v2/ability/" + ability
     if not check_status(url):
-        await ctx.channel.send("something went wrong")
+        await ctx.channel.send("That's not an ability or a pokemon, check your spelling/spacing")
         return -1
 
     return requests.get(url).json()
